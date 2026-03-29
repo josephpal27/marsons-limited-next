274be2c5924a9e132e069f713d184729789bf16c
Joseph Pal <josephpal27@gmail.com>
2026-03-29 21:30:55 +0530

    Home Banner Updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
+import HomeBanner from "@/components/home/HomeBanner";
 
 export default function Home() {
   return (
     <>
-      <h1 className="text-red-700 text-[3rem]">
-        Home Page
-      </h1>
+      <HomeBanner />
     </>
   );
 }
