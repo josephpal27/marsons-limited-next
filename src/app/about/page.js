7e21e67f67f420d2f6c67bf3da0950930162d370
Joseph Pal <josephpal27@gmail.com>
2026-03-30 23:48:37 +0530

    About Page Updated

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -4,10 +4,12 @@ import Highlights from "@/components/about/Highlights";
 // import OurClients from "@/components/about/OurClients";
 // import OurJourney from "@/components/about/OurJourney";
 import VisionMission from "@/components/about/VisionMission";
+import MyNavbar from "@/components/Navbar";
 
 export default function About() {
     return (
         <>
+            <MyNavbar />
             <AboutBanner />
             <AboutWhatWeDo />
             <Highlights />
