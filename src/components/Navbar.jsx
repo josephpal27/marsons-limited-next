aa1ca24841700c35c4b603a80eb304d38a84847f
Joseph Pal <josephpal27@gmail.com>
2026-03-30 23:48:15 +0530

    Navbar Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import Navbar from "react-bootstrap/Navbar";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import Link from "next/link";
+import Image from "next/image";
 import "./Navbar.css";
 
 const MyNavbar = () => {
@@ -16,8 +17,14 @@ const MyNavbar = () => {
 
   return (
     <Navbar expand="lg" className="navbar">
-      <Navbar.Brand as={Link} to="/" className="nav-brand">
-        <img src={logo} alt="logo" loading="eager" />
+      <Navbar.Brand as={Link} href="/" className="nav-brand">
+        <Image
+          src="/images/logo/logo.png"
+          alt="Marsons Limited"
+          width={100}
+          height={30}
+          className="w-full h-auto"
+        />
       </Navbar.Brand>
 
       {/* Toggle button opens Offcanvas */}
@@ -32,82 +39,88 @@ const MyNavbar = () => {
       >
         <Offcanvas.Header>
           <Offcanvas.Title id="offcanvasNavbarLabel-expand-lg">
-            <img src={logo} alt="logo" className="offCanvasLogo" />
+            <Image
+              src="/images/logo/logo.png"
+              alt="Marsons Limited"
+              width={100}
+              height={30}
+              className="w-full h-auto offCanvasLogo"
+            />
           </Offcanvas.Title>
         </Offcanvas.Header>
 
         <Offcanvas.Body>
           <Nav className="justify-content-end flex-grow-1">
-            <Nav.Link as={NavLink} to="/" onClick={handleClose}>
+            <Nav.Link as={Link} href="/" onClick={handleClose}>
               HOME
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/about" onClick={handleClose}>
+            <Nav.Link as={Link} href="/about" onClick={handleClose}>
               ABOUT
             </Nav.Link>
             {/* Bootstrap Dropdown */}
             <NavDropdown title="PRODUCTS" id="navbarDropdown">
-              <NavDropdown.Item as={NavLink} to="/products/distribution-transformers" onClick={handleClose}>
+              <NavDropdown.Item as={Link} href="/products/distribution-transformers" onClick={handleClose}>
                 Distribution Transformers
               </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/products/power-transformers" onClick={handleClose}>
+              <NavDropdown.Item as={Link} href="/products/power-transformers" onClick={handleClose}>
                 Power Transformers
               </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/products/furnace-duty-transformers" onClick={handleClose}>
+              <NavDropdown.Item as={Link} href="/products/furnace-duty-transformers" onClick={handleClose}>
                 Furnace Duty Transformers
               </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/products/dry-type-transformers" onClick={handleClose}>
+              <NavDropdown.Item as={Link} href="/products/dry-type-transformers" onClick={handleClose}>
                 Dry Type Transformers
               </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/products/pad-mounted-transformers" onClick={handleClose}>
+              <NavDropdown.Item as={Link} href="/products/pad-mounted-transformers" onClick={handleClose}>
                 PAD Mounted Transformers
               </NavDropdown.Item>
-              {/* <NavDropdown.Item as={NavLink} to="/products/cast-resin-transformers" onClick={handleClose}>
+              {/* <NavDropdown.Item as={NavLink} href="/products/cast-resin-transformers" onClick={handleClose}>
                 Cast resin transformers
               </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/products/solar-transformers" onClick={handleClose}>
+              <NavDropdown.Item as={NavLink} href="/products/solar-transformers" onClick={handleClose}>
                 Solar transformers
               </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/products/instrumental-transformers" onClick={handleClose}>
+              <NavDropdown.Item as={NavLink} href="/products/instrumental-transformers" onClick={handleClose}>
                 Instrumental transformers
               </NavDropdown.Item> */}
             </NavDropdown>
-            {/* <Nav.Link as={NavLink} to="/clientele" onClick={handleClose}>
+            {/* <Nav.Link as={NavLink} href="/clientele" onClick={handleClose}>
               CLIENTELE
             </Nav.Link> */}
             {/* Bootstrap Dropdown */}
             <NavDropdown title="INVESTOR CORNER" id="navbarDropdown">
-              <NavDropdown.Item as={NavLink} to="/investor-corner/financials" onClick={handleClose}>
+              <NavDropdown.Item as={Link} href="/investor-corner/financials" onClick={handleClose}>
                 Financials
               </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/investor-corner/regulation" onClick={handleClose}>
+              <NavDropdown.Item as={Link} href="/investor-corner/regulation" onClick={handleClose}>
                 Regulation 46(2)
               </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/investor-corner/corporate-governance" onClick={handleClose}>
+              <NavDropdown.Item as={Link} href="/investor-corner/corporate-governance" onClick={handleClose}>
                 Corporate Governance
               </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/investor-corner/shareholding-pattern" onClick={handleClose}>
+              <NavDropdown.Item as={Link} href="/investor-corner/shareholding-pattern" onClick={handleClose}>
                 Shareholding Pattern
               </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/investor-corner/notices-and-results" onClick={handleClose}>
+              <NavDropdown.Item as={Link} href="/investor-corner/notices-and-results" onClick={handleClose}>
                 Notices & Results
               </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/investor-corner/policies" onClick={handleClose}>
+              <NavDropdown.Item as={Link} href="/investor-corner/policies" onClick={handleClose}>
                 Policies
               </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/investor-corner/unclaimed-dividend" onClick={handleClose}>
+              <NavDropdown.Item as={Link} href="/investor-corner/unclaimed-dividend" onClick={handleClose}>
                 Unclaimed Dividend
               </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/investor-corner/contact-details" onClick={handleClose}>
+              <NavDropdown.Item as={Link} href="/investor-corner/contact-details" onClick={handleClose}>
                 Contact Details
               </NavDropdown.Item>
             </NavDropdown>
-            {/* <Nav.Link as={NavLink} to="/csr" onClick={handleClose}>
+            {/* <Nav.Link as={NavLink} href="/csr" onClick={handleClose}>
               CSR
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/career" onClick={handleClose}>
+            <Nav.Link as={NavLink} href="/career" onClick={handleClose}>
               CAREER
             </Nav.Link> */}
-            <Nav.Link as={NavLink} to="/contact" onClick={handleClose}>
+            <Nav.Link as={Link} href="/contact" onClick={handleClose}>
               CONTACT
             </Nav.Link>
           </Nav>
