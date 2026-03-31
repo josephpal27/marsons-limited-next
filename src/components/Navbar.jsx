33737f054d5c2cd5e2fbe32be3e46c859b10d996
Joseph Pal <josephpal27@gmail.com>
2026-04-01 00:13:35 +0530

    Navbar Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,13 +6,15 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import Navbar from "react-bootstrap/Navbar";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import "./Navbar.css";
 
 const MyNavbar = () => {
-  const [show, setShow] = useState(false);
 
+  const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const pathname = usePathname();
 
   return (
     <Navbar expand="lg" className="navbar">
@@ -38,76 +40,76 @@ const MyNavbar = () => {
 
         <Offcanvas.Body>
           <Nav className="justify-content-end flex-grow-1">
-            <Nav.Link as={Link} href="/" onClick={handleClose}>
+            <Nav.Link as={Link} href="/" onClick={handleClose} className={`${pathname === "/" ? "active" : ""}`}>
               HOME
             </Nav.Link>
-            <Nav.Link as={Link} href="/about" onClick={handleClose}>
+            <Nav.Link as={Link} href="/about" onClick={handleClose} className={`${pathname === "/about" ? "active" : ""}`}>
               ABOUT
             </Nav.Link>
             {/* Bootstrap Dropdown */}
             <NavDropdown title="PRODUCTS" id="navbarDropdown">
-              <NavDropdown.Item as={Link} href="/products/distribution-transformers" onClick={handleClose}>
+              <NavDropdown.Item as={Link} href="/products/distribution-transformers" onClick={handleClose} className={`${pathname === "/products/distribution-transformers" ? "active" : ""}`}>
                 Distribution Transformers
               </NavDropdown.Item>
-              <NavDropdown.Item as={Link} href="/products/power-transformers" onClick={handleClose}>
+              <NavDropdown.Item as={Link} href="/products/power-transformers" onClick={handleClose} className={`${pathname === "/products/power-transformers" ? "active" : ""}`}>
                 Power Transformers
               </NavDropdown.Item>
-              <NavDropdown.Item as={Link} href="/products/furnace-duty-transformers" onClick={handleClose}>
+              <NavDropdown.Item as={Link} href="/products/furnace-duty-transformers" onClick={handleClose} className={`${pathname === "/products/furnace-duty-transformers" ? "active" : ""}`}>
                 Furnace Duty Transformers
               </NavDropdown.Item>
-              <NavDropdown.Item as={Link} href="/products/dry-type-transformers" onClick={handleClose}>
+              <NavDropdown.Item as={Link} href="/products/dry-type-transformers" onClick={handleClose} className={`${pathname === "/products/dry-type-transformers" ? "active" : ""}`}>
                 Dry Type Transformers
               </NavDropdown.Item>
-              <NavDropdown.Item as={Link} href="/products/pad-mounted-transformers" onClick={handleClose}>
+              <NavDropdown.Item as={Link} href="/products/pad-mounted-transformers" onClick={handleClose} className={`${pathname === "/products/pad-mounted-transformers" ? "active" : ""}`}>
                 PAD Mounted Transformers
               </NavDropdown.Item>
-              {/* <NavDropdown.Item as={NavLink} href="/products/cast-resin-transformers" onClick={handleClose}>
+              {/* <NavDropdown.Item as={NavLink} href="/products/cast-resin-transformers" onClick={handleClose} className={`${pathname === "/products/cast-resin-transformers" ? "active" : ""}`}>
                 Cast resin transformers
               </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} href="/products/solar-transformers" onClick={handleClose}>
+              <NavDropdown.Item as={NavLink} href="/products/solar-transformers" onClick={handleClose} className={`${pathname === "/products/solar-transformers" ? "active" : ""}`}>
                 Solar transformers
               </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} href="/products/instrumental-transformers" onClick={handleClose}>
+              <NavDropdown.Item as={NavLink} href="/products/instrumental-transformers" onClick={handleClose} className={`${pathname === "/products/instrumental-transformers" ? "active" : ""}`}>
                 Instrumental transformers
               </NavDropdown.Item> */}
             </NavDropdown>
-            {/* <Nav.Link as={NavLink} href="/clientele" onClick={handleClose}>
+            {/* <Nav.Link as={NavLink} href="/clientele" onClick={handleClose} className={`${pathname === "/clientele" ? "active" : ""}`}>
               CLIENTELE
             </Nav.Link> */}
             {/* Bootstrap Dropdown */}
             <NavDropdown title="INVESTOR CORNER" id="navbarDropdown">
-              <NavDropdown.Item as={Link} href="/investor-corner/financials" onClick={handleClose}>
+              <NavDropdown.Item as={Link} href="/investor-corner/financials" onClick={handleClose} className={`${pathname === "/investor-corner/financials" ? "active" : ""}`}>
                 Financials
               </NavDropdown.Item>
-              <NavDropdown.Item as={Link} href="/investor-corner/regulation" onClick={handleClose}>
+              <NavDropdown.Item as={Link} href="/investor-corner/regulation" onClick={handleClose} className={`${pathname === "/investor-corner/regulation" ? "active" : ""}`}>
                 Regulation 46(2)
               </NavDropdown.Item>
-              <NavDropdown.Item as={Link} href="/investor-corner/corporate-governance" onClick={handleClose}>
+              <NavDropdown.Item as={Link} href="/investor-corner/corporate-governance" onClick={handleClose} className={`${pathname === "/investor-corner/corporate-governance" ? "active" : ""}`}>
                 Corporate Governance
               </NavDropdown.Item>
-              <NavDropdown.Item as={Link} href="/investor-corner/shareholding-pattern" onClick={handleClose}>
+              <NavDropdown.Item as={Link} href="/investor-corner/shareholding-pattern" onClick={handleClose} className={`${pathname === "/investor-corner/shareholding-pattern" ? "active" : ""}`}>
                 Shareholding Pattern
               </NavDropdown.Item>
-              <NavDropdown.Item as={Link} href="/investor-corner/notices-and-results" onClick={handleClose}>
+              <NavDropdown.Item as={Link} href="/investor-corner/notices-and-results" onClick={handleClose} className={`${pathname === "/investor-corner/notices-and-results" ? "active" : ""}`}>
                 Notices & Results
               </NavDropdown.Item>
-              <NavDropdown.Item as={Link} href="/investor-corner/policies" onClick={handleClose}>
+              <NavDropdown.Item as={Link} href="/investor-corner/policies" onClick={handleClose} className={`${pathname === "/investor-corner/policies" ? "active" : ""}`}>
                 Policies
               </NavDropdown.Item>
-              <NavDropdown.Item as={Link} href="/investor-corner/unclaimed-dividend" onClick={handleClose}>
+              <NavDropdown.Item as={Link} href="/investor-corner/unclaimed-dividend" onClick={handleClose} className={`${pathname === "/investor-corner/unclaimed-dividend" ? "active" : ""}`}>
                 Unclaimed Dividend
               </NavDropdown.Item>
-              <NavDropdown.Item as={Link} href="/investor-corner/contact-details" onClick={handleClose}>
+              <NavDropdown.Item as={Link} href="/investor-corner/contact-details" onClick={handleClose} className={`${pathname === "/investor-corner/contact-details" ? "active" : ""}`}>
                 Contact Details
               </NavDropdown.Item>
             </NavDropdown>
-            {/* <Nav.Link as={NavLink} href="/csr" onClick={handleClose}>
+            {/* <Nav.Link as={NavLink} href="/csr" onClick={handleClose} className={`${pathname === "/csr" ? "active" : ""}`}>
               CSR
             </Nav.Link>
-            <Nav.Link as={NavLink} href="/career" onClick={handleClose}>
+            <Nav.Link as={NavLink} href="/career" onClick={handleClose} className={`${pathname === "/career" ? "active" : ""}`}>
               CAREER
             </Nav.Link> */}
-            <Nav.Link as={Link} href="/contact" onClick={handleClose}>
+            <Nav.Link as={Link} href="/contact" onClick={handleClose} className={`${pathname === "/contact" ? "active" : ""}`}>
               CONTACT
             </Nav.Link>
           </Nav>
--- src/components/contact/ContactMap.jsx
+++ src/components/contact/ContactMap.jsx
@@ -8,7 +8,7 @@ const ContactMap = () => {
                 h-[35vh] sm:h-[60vh]
                 mt-[2.5rem] sm:mt-[4rem] lg:mt-[4.2rem] xl:mt-[4.7rem] 2xl:mt-[5rem]
             ">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58972.07628788787!2d88.1906070486328!3d22.513382300000007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a027bb8738bd239%3A0x7619639bf41020a2!2sMarsons%20Limited!5e0!3m2!1sen!2sin!4v1773315825117!5m2!1sen!2sin" width="100%" height="100%" allowFullScreen loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58972.07628788787!2d88.1906070486328!3d22.513382300000007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a027bb8738bd239%3A0x7619639bf41020a2!2sMarsons%20Limited!5e0!3m2!1sen!2sin!4v1773315825117!5m2!1sen!2sin" width="100%" height="100%" allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
             </section>
         </>
     )
